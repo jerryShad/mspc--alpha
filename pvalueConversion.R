884423405f2195779caf07576bdc07952a46ebee
Julaiti <j.shahidin@gmail.com>
2016-07-12 16:53:28 +0200

    utility function for pvalue conversion

---
+++ pvalueConversion.R
@@ -0,0 +1,19 @@
+#' pvalueConversion
+#' @param peakFile input bed file
+#' @param pvalueBase cofficient for different type of pvalue standard
+#' @return GRanges objects
+#' @export
+#' @importFrom rtracklayer score
+#' @author Julaiti Shayiding
+
+pvalueConversion <- function(x, pvalueBase = 1L) {
+  stopifnot(class(x) == "GRanges")
+  # explore score of all features
+  if(is.null(x$pvalue)){
+    x$pvalue <- 10^(score(x)/(- pvalueBase))
+    x$score <- NULL
+  } else {
+    x
+  }
+  return(x)
+}
